364abf34a6f406b4a0c076d750c9999793a0c0b3
Mateo Restrepo <marestrepohi@unal.edu.co>
2025-06-04 09:57:11 -0500

    style: mejorar el responsive

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,10 @@ import { Toaster } from './components/ui/toaster';
 const App: React.FC = () => {
   return (
     <VoiceAssistantProvider>
-      <div className="min-h-screen flex items-center justify-center p-4">
-        <VoiceAssistant />
+      <div className="min-h-screen flex items-center justify-center w-full px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+        <div className="w-full max-w-[90vw]">
+          <VoiceAssistant />
+        </div>
       </div>
       <Toaster />
     </VoiceAssistantProvider>
